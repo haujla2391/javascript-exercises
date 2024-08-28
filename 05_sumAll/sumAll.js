f5aae6ba0e3d17e898f6fa2b4bdd46295b139078
Harman Aujla <harman11524@gmail.com>
2024-08-28 12:14:50 -0400

    Did exercises 1-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(lower, higher) {
+    if (!Number.isInteger(lower) || !Number.isInteger(higher)) return "ERROR";
+    if (lower < 0 || higher < 0) return "ERROR";
+    if (lower > higher) {
+      const temp = lower;
+      lower = higher;
+      higher = temp;
+    }
 
+    let result = 0
+    for(let i = lower; i <= higher; ++i){
+        result += i
+    }
+    return result
 };
 
 // Do not edit below this line
